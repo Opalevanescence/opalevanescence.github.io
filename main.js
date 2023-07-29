78395e26a16142b60f44a04adaab10e1e1983c4e
Jessica DeWitt <jessicaadewitt@gmail.com>
2023-07-29 15:28:40 -0700

    first pass at website

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ function shadeNav(){
     nav = document.getElementById("navigation");
     if(window.innerWidth >= 550){
         if(window.pageYOffset >= 700-46){
-            nav.style.background = "rgba(0, 0, 0, 1)"
+            nav.style.background = "rgba(69, 108, 147, 1)"
         }else{
-            nav.style.background= "rgba(0, 0, 0, 0.5)"
+            nav.style.background= "rgba(69, 108, 147, 0.5)"
         }
     }else{
         if(window.pageYOffset >= 350-92){
-            nav.style.background = "rgba(0, 0, 0, 1)"
+            nav.style.background = "rgba(69, 108, 147, 1)"
         }else{
-            nav.style.background= "rgba(0, 0, 0, 0.5)"
+            nav.style.background= "rgba(69, 108, 147, 0.5)"
         }
     }
 }
